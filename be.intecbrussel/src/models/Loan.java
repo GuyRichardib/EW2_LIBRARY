ad8b7e67b63ef882b668d99c41f0bce10541a39a
Richard <ibambasi.guyrichard@gmail.com>
2025-09-01 13:41:31 +0200

    first commit

---
+++ be.intecbrussel/src/models/Loan.java
@@ -0,0 +1,15 @@
+package models;
+
+import java.time.LocalDate;
+
+
+public class Loan {
+    private Book book;
+    private Member member;
+    private LocalDate loanDate;
+    private LocalDate dueDate;
+    private LocalDate returnDate;
+
+    public Loan() {
+    }
+}
---
+++ be.intecbrussel/src/models/Member.java
@@ -0,0 +1,15 @@
+package models;
+
+
+public class Member {
+    private String name;
+    private int age;
+    private String membershipNumber;
+    private String contactInfo;
+
+    public Member() {
+
+    }
+
+
+}
